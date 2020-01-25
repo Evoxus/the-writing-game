0272bfce4a38cc69042975565e08c34824e48a79
Michael Ploughman <michael@michaelhploughman.com>
2020-01-25 06:53:05 -0700

    Switching to one input per line of text

--- src/Page/Page.js
+++ src/Page/Page.js
@@ -4,11 +4,11 @@ import './Page.css';
 export default function Page(props) {
   // will need the handleTurn function passed down
   return (
-    <div className="outerDiv">
-      <div data-page-number="1">
-        <textarea className='pageBody'>
-
-        </textarea>
+    <div className='outerDiv'>
+      <div data-page-number='1'>
+        {/* <textarea className='pageBody'></textarea> */}
+        <input type='text' className='pageBody' name='lineOne' id='lineOne' />
+        <input type='text' className='pageBody' name='lineTwo' id='lineTwo' />
       </div>
     </div>
   )
