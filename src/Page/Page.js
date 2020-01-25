d288f8384d8b961e2009f762118c72f7dd4620b7
Michael Ploughman <michael@michaelhploughman.com>
2020-01-25 07:12:11 -0700

    Merged changes for two input boxes

--- src/Page/Page.js
+++ src/Page/Page.js
@@ -4,20 +4,19 @@ import './Page.css';
 
 export default function Page(props) {
   return (
-    <form className="outerDiv" id='storyForm' onSubmit={(e) => props.handleTurn(e)}>
-        {/* <textarea className='pageBody'></textarea> */}
-        <input type='text' className='pageBody' name='lineOne' id='lineOne' />
-        <input type='text' className='pageBody' name='lineTwo' id='lineTwo' />
-      <div className='flexContainer'>
+    <div className='flexContainer'>
+      <form className="outerDiv" id='storyForm' onSubmit={(e) => props.handleTurn(e)}>
+        <div>
+          <input type='text' className='pageBody' name='lineOne' id='lineOne' />
+          <input type='text' className='pageBody' name='lineTwo' id='lineTwo' />
+        </div>
         {/* two input boxes instead of text area? could get around mobile limitation */}
         {/* <textarea row='2' col='66' className='pageBody' form='storyForm' id='storyInput' 
           name='storyInput' defaultValue={props.story.length >= 1 ? props.story[props.story.length - 1] : ''}>
         </textarea> */}
-        <PlayerList playerList={props.playerList}/>
-      </div>
-      <button type='submit' id='endTurn'>End your turn</button>
-    </form>
-    
-    
+        <button type='submit' id='endTurn'>End your turn</button>
+      </form>
+      <PlayerList playerList={props.playerList} />
+    </div>
   )
 }
